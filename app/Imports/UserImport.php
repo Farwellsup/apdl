46186e18487bc9fdda4b8ae0cbff6fadccbde4c5
Wanjaiya <kangethewanjaiya@gmail.com>
2026-04-23 14:36:34 +0300

    chunk data

--- app/Imports/UserImport.php
+++ app/Imports/UserImport.php
@@ -4,11 +4,18 @@ namespace App\Imports;
 
 use Maatwebsite\Excel\Concerns\ToArray;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
 
-class UserImport implements ToArray, WithHeadingRow
+class UserImport implements ToArray, WithHeadingRow, WithChunkReading
 {
     public function array(array $rows)
     {
         return $rows;
     }
+
+    public function chunkSize(): int
+    {
+        return 100;
+    }
+
 }
