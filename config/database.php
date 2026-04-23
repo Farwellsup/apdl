03119ce4e5f0cd51ce5c0ba508e791aa07ed7c36
Wanjaiya <kangethewanjaiya@gmail.com>
2026-04-23 13:21:24 +0300

    user config

--- config/database.php
+++ config/database.php
@@ -62,6 +62,25 @@ return [
             ]) : [],
         ],
 
+        'edx_mysql' => [
+            'driver' => 'mysql',
+            'host' => env('EDX_DB_HOST', '127.0.0.1'),
+            'port' => env('EDX_DB_PORT', '3306'),
+            'database' => env('EDX_DB_DATABASE', 'edxapp'),
+            'username' => env('EDX_DB_USERNAME', 'root'),
+            'password' => env('EDX_DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
