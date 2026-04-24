b967775576f13d4c81e0737ddc3631d80506477f
Wanjaiya <kangethewanjaiya@gmail.com>
2026-04-24 15:02:10 +0300

    Diffrent take on user upload

--- app/Services/EdxUserImportService.php
+++ app/Services/EdxUserImportService.php
@@ -20,181 +20,126 @@ class EdxUserImportService
         $lastIdFile = storage_path('app/last_id_student_edx.txt');
         $lastId = file_exists($lastIdFile) ? (int) file_get_contents($lastIdFile) : 0;
         // dd($lastId);
-        $latestRecord = EdxAuthUser::orderBy('id', 'desc')->first();
+        // $latestRecord = EdxAuthUser::orderBy('id', 'desc')->first();
 
-        $latestId = $latestRecord->id ?? 0;
+        // $latestId = $latestRecord->id ?? 0;
 
          $total = EdxAuthUser::count();
 
+       // $total = 0;
+
         $token = JwtHelper::generateToken();
 
-        $url = 'https://transform.thebrandinside.com/api/edxUsers?lastId=' . $latestId;
-
-        $response = Http::withHeaders([
-            'Authorization' => 'Bearer ' . $token,
-        ])->get("$url");
 
 
         try {
-            if ($response->successful()) {
-                $data = collect($response->json())
-                    ->reverse()  // newest first
-                    ->values()       // reset array keys for upsert
-                    ->toArray();     // convert to plain array
 
-                $apiCount = count($data);
 
-                foreach (array_chunk($data, 500) as $chunk) {
+            foreach ($rows as $user) {
 
-                    foreach ($rows as $user) {
+                $url = 'https://transform.thebrandinside.com/api/edxUsers/' . $user['username'];
 
-                        if ($chunk) {
-                            $edxUsers = array_map(function ($item) use ($user) {
-
-                                if ($item['username'] === $user['username']) {
-                                    return [
-                                        'id'                         => $item['id'],
-                                        'password'                   => $user['password'],
-                                        'is_superuser'               => $item['is_superuser'],
-                                        'username'                   => $item['username'],
-                                        'first_name'                 => ($item['username']) ? $user['username'] : $user['first_name'],
-                                        'last_name'                  => ($item['username']) ? $user['username'] : $user['first_name'],
-                                        'email'                      => $user['email'],
-                                        'is_staff'                   => $item['is_staff'],
-                                        'is_active'                  => $item['is_active'],
-                                        'last_login'                 => $item['last_login']
-                                            ? Carbon::parse($item['last_login'])->toDateTimeString()
-                                            : null,
-
-                                        'date_joined'               => $item['date_joined']
-                                            ? Carbon::parse($item['date_joined'])->toDateTimeString()
-                                            : null,
-
-                                    ];
-                                }
-                            }, $chunk);
+                $response = Http::withHeaders([
+                    'Authorization' => 'Bearer ' . $token,
+                ])->get("$url");
 
 
 
-                            $edxUserProfiles = array_map(function ($item) use ($user) {
-                                if (!empty($item['profile'])) {
-                                    if ($item['username'] === $user['username']) {
-                                        return [
-                                            'id'                       => $item['profile']['id'],
-                                            'name'                     => $item['profile']['name'],
-                                            'meta'                     => $item['profile']['meta'],
-                                            'courseware'               => $item['profile']['courseware'],
-                                            'language'                 => $item['profile']['language'],
-                                            'location'                 => $item['profile']['location'],
-                                            'year_of_birth'            => $item['profile']['year_of_birth'],
-                                            'gender'                   => $item['profile']['gender'],
-                                            'level_of_education'       => $item['profile']['level_of_education'],
-                                            'mailing_address'          => $item['profile']['mailing_address'],
-                                            'city'                     => $item['profile']['city'],
-                                            'country'                  => $item['profile']['country'],
-                                            'goals'                    => $item['profile']['goals'],
-                                            'allow_certificate'        => $item['profile']['allow_certificate'],
-                                            'bio'                      => $item['profile']['bio'],
-                                            'profile_image_uploaded_at'       => $item['profile']['profile_image_uploaded_at'],
-                                            'user_id'                   => $item['profile']['user_id'],
-                                        ];
-                                    }
-                                }
-                            }, $chunk);
+                if ($response->successful()) {
+                    $data = collect($response->json())
+                        ->reverse()  // newest first
+                        ->values()       // reset array keys for upsert
+                        ->toArray();  // Process the data as needed
+                    foreach ($data as $item) {
 
-
-                            $edxUserRegistrations = array_map(function ($item) use ($user) {
-                                if (!empty($item['auth_registration'])) {
-                                    if ($item['username'] === $user['username']) {
-                                        return [
-                                            'id'                       => $item['auth_registration']['id'],
-                                            'activation_key'           => $item['auth_registration']['activation_key'],
-                                            'user_id'                  => $item['auth_registration']['user_id'],
-                                        ];
-                                    }
-                                }
-                            }, $chunk);
+                        $edxUsers = [
+                            'id' => $item['id'],
+                            'password' => $item['password'],
+                            'is_superuser' => $item['is_superuser'],
+                            'username' => $item['username'],
+                            'first_name' => $item['first_name'],
+                            'last_name' => $item['last_name'],
+                            'email' => $item['email'],
+                            'is_staff' => $item['is_staff'],
+                            'is_active' => $item['is_active'],
+                            'last_login' => Carbon::parse($item['last_login']),
+                            'date_joined' => Carbon::parse($item['date_joined']),
+                        ];
 
 
 
-                            EdxAuthUser::upsert(
-                                $edxUsers,
-                                ['id'], // unique key
-                                [
-                                    // Columns to update if record already exists
-                                    'password',
-                                    'is_superuser',
-                                    'username',
-                                    'first_name',
-                                    'last_name',
-                                    'email',
-                                    'is_staff',
-                                    'is_active',
-                                    'last_login',
-                                    'date_joined',
-                                ]
-                            );
+                        EdxAuthUser::upsert(
+                            $edxUsers,
+                            ['id'], // unique key
+                            [
+                                // Columns to update if record already exists
+                                'password',
+                                'is_superuser',
+                                'username',
+                                'first_name',
+                                'last_name',
+                                'email',
+                                'is_staff',
+                                'is_active',
+                                'last_login',
+                                'date_joined',
+                            ]
+                        );
 
 
-                            // Perform bulk upsert
-                            AuthUserprofile::upsert(
-                                $edxUserProfiles,
-                                ['id'], // unique key
-                                [
-                                    // Columns to update if record already exists
-                                    'name',
-                                    'meta',
-                                    'courseware',
-                                    'language',
-                                    'location',
-                                    'year_of_birth',
-                                    'gender',
-                                    'level_of_education',
-                                    'mailing_address',
-                                    'city',
-                                    'country',
-                                    'goals',
-                                    'allow_certificate',
-                                    'bio',
-                                    'profile_image_uploaded_at',
-                                    'user_id',
-                                ]
-                            );
+                        // Perform bulk upsert
+                        AuthUserprofile::upsert(
+                            $item['profile'],
+                            ['id'], // unique key
+                            [
+                                // Columns to update if record already exists
+                                'name',
+                                'meta',
+                                'courseware',
+                                'language',
+                                'location',
+                                'year_of_birth',
+                                'gender',
+                                'level_of_education',
+                                'mailing_address',
+                                'city',
+                                'country',
+                                'goals',
+                                'allow_certificate',
+                                'bio',
+                                'profile_image_uploaded_at',
+                                'user_id',
+                            ]
+                        );
 
+                        // Perform bulk upsert
+                        AuthRegistration::upsert(
+                            $item['auth_registration'],
+                            ['id'], // unique key
+                            [
+                                // Columns to update if record already exists
+                                'activation_key',
+                                'user_id',
+                            ]
+                        );
 
-
-
-                            // Perform bulk upsert
-                            AuthRegistration::upsert(
-                                $edxUserRegistrations,
-                                ['id'], // unique key
-                                [
-                                    // Columns to update if record already exists
-                                    'activation_key',
-                                    'user_id',
-                                ]
-                            );
-                        }
+                        $total += 1;
+                        file_put_contents($lastIdFile,  $lastId + $total);
                     }
+
+
+                    return [
+                        'status' => true,
+                        'message' => "Successfully imported {$total} users from API.",
+                        'total_imported' => $total,
+                    ];
+                } else {
+                    // Handle the error response
+                    $errors[] = [
+                        'username' => $user['username'],
+                        'error' => "Failed to fetch data for username: {$user['username']}. Status: {$response->status()}",
+                    ];
                 }
-
-
-                   $total+=$apiCount;
-                   file_put_contents($lastIdFile,  $lastId+$apiCount);
-
-                return [
-                    'status' => true,
-                    'message' => "Successfully imported {$apiCount} users from API.",
-                    'total_imported' => $total,
-                ];
-
-            } else {
-                $statusCode = $response->status();
-                $errorMessage = $response->body();
-                return [
-                    'status' => false,
-                    'message' => "API Error: {$statusCode} - {$errorMessage}",
-                ];
             }
         } catch (\Exception $e) {
 
